af0bbd07be4665e62ed2ecd8ef2a89f50c1df157
kokodavid <kingkgdave@gmail.com>
2019-09-19 07:46:10 +0300

    second commit

--- app/src/main/java/com/david/moveme/HomeActivity.java
+++ app/src/main/java/com/david/moveme/HomeActivity.java
@@ -33,7 +33,7 @@ public class HomeActivity extends AppCompatActivity {
         user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,CustomerLoginActivity.class);
+                Intent intent = new Intent(HomeActivity.this,WelcomeActivity.class);
                 startActivity(intent);
                 finish();
                 return;
